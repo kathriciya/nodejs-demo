228bbb74688a656f5a0cba05e38eac6c83561856
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-20 22:02:20 +0700

    response methods

--- index.js
+++ index.js
@@ -3,27 +3,10 @@ import express from 'express';
 const port = 8000;
 const app = express();
 
-app.all('/hello', (req, res, next) => {
-	console.log('All');
-	next();
+app.get('/hello', (req, res) => {
+	res.status(404).end();
 });
 
-const cb = (req, res, next) => {
-	console.log('CB');
-	next();
-}
-
-// app.get('/hello',cb, (req, res) => {
-// 	res.send('Привет!');
-// });
-
-app.route('/user')
-	.get('/hello', (req, res) => {
-		res.send('Привет!');
-	})
-	.post('/hello', (req, res) => {
-		res.send('Привет POST!');
-	});
 
 
 app.listen(port, ()=>{
