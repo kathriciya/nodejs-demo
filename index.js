703feb0d7557d9fa2dd4de7a7f4659268218b303
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-20 23:32:48 +0700

    add router

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import { userRouter } from './users/users.js';
 
 const port = 8000;
 const app = express();
 
 app.get('/hello', (req, res) => {
-	res.status(404).end();
+	// res.send('Привет!');
+	res.end();
 });
 
+app.use('/users', userRouter);
 
 
 app.listen(port, ()=>{
