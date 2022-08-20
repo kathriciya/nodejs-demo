f0937f3c47c45f8ac489455b57c9e7aa7dc487f2
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-20 20:16:16 +0700

    use express

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-import http from 'http';
+import express from 'express';
 
-const host = '127.0.0.1';
 const port = 8000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-	switch (req.method) {
-		case 'GET':
-			switch (req.url) {
-				case '/hello':
-					res.statusCode = 200;
-					res.setHeader('Content-Type', 'text/plain');
-					res.end('Привет!');
-					break;
-			}
-			break;
-	}
-});
-
-server.listen(port, host, () => {
-	console.log(`Сервер запущен на ${host}:${port}`);
-});
+app.get('/hello', (req, res)=>{
+	res.send('Привет!');
+})
+app.listen(port, ()=>{
+	console.log(`Сервер запущен на http://localhost:${port}`);
+})
