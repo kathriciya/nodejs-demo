4432636099c9decd4202bdffbd0e800e642fec17
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-20 20:55:15 +0700

    routing

--- index.js
+++ index.js
@@ -3,9 +3,29 @@ import express from 'express';
 const port = 8000;
 const app = express();
 
-app.get('/hello', (req, res)=>{
-	res.send('Привет!');
-})
+app.all('/hello', (req, res, next) => {
+	console.log('All');
+	next();
+});
+
+const cb = (req, res, next) => {
+	console.log('CB');
+	next();
+}
+
+// app.get('/hello',cb, (req, res) => {
+// 	res.send('Привет!');
+// });
+
+app.route('/user')
+	.get('/hello', (req, res) => {
+		res.send('Привет!');
+	})
+	.post('/hello', (req, res) => {
+		res.send('Привет POST!');
+	});
+
+
 app.listen(port, ()=>{
 	console.log(`Сервер запущен на http://localhost:${port}`);
 })
