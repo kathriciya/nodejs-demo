3902e9104e657f43ead82789ed6adeb4f8577e31
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-24 23:52:27 +0700

    add data transfer object

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { ExeptionFilter } from './errors/exeption.filter';
 import { ILogger } from './logger/logger.interface';
 import { TYPES } from './types';
 import 'reflect-metadata';
+import { json } from 'body-parser';
 @injectable()
 export class App {
 	app: Express;
@@ -21,6 +22,10 @@ export class App {
 		this.port = 8000;
 	}
 
+	useMiddleware(): void {
+		this.app.use(json());
+	}
+
 	useRoutes(): void {
 		this.app.use('/users', this.userController.router);
 	}
@@ -30,6 +35,7 @@ export class App {
 	}
 
 	public async init(): Promise<void> {
+		this.useMiddleware();
 		this.useRoutes();
 		this.useExeptionFilters();
 		this.server = this.app.listen(this.port);
